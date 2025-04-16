34fe4f95af268a25545b17531cc0c85b11605f95
Rahul2026-Web <154739382+Rahul2026-Web@users.noreply.github.com>
2025-04-16 11:35:36 +0530

    Update utils.py

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+
+import pickle
+import os
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise Exception(f"Error saving object: {e}")
